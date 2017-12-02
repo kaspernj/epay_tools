b910e2dce2a287b6a7e9ff79ce8a091d518347ce
kaspernj <kaspernj@gmail.com>
2017-12-02 20:07:50 +0000

    Fixed various issues

--- lib/epay_tools/hash_generator.rb
+++ lib/epay_tools/hash_generator.rb
@@ -1,18 +1,33 @@
 class EpayTools::HashGenerator < ServicePattern::Service
   attr_reader :epay_md5_key, :params
 
+  KEYS_ORDER = %w[txnid orderid amount currency date time txnfee paymenttype cardno hash].freeze
+
   def initialize(epay_md5_key:, params:)
     @epay_md5_key = epay_md5_key
     @params = params.reject { |key, _value| key.to_s == "hash" }
   end
 
   def execute!
-    ServicePattern::Response.new(result: hash)
+    ServicePattern::Response.new(result: calculate_hash)
   end
 
 private
 
-  def hash
-    Digest::MD5.hexdigest("#{params.values.join}#{epay_md5_key}")
+  def calculate_hash
+    @_calculate_hash ||= Digest::MD5.hexdigest("#{params_values.join}#{epay_md5_key}")
+  end
+
+  def params_values
+    unless @_params_values
+      @_params_values = {}
+      KEYS_ORDER.each do |key|
+        @_params_values[key] = params.delete(key) if params.key?(key)
+      end
+
+      @_params_values.merge!(params)
+    end
+
+    @_params_values.values
   end
 end
