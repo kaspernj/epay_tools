2f65fab994791779e63058bd9e632b040375802e
kaspernj <kaspernj@gmail.com>
2018-04-16 12:01:23 +0000

    Fixed hash and response generators

--- lib/epay_tools/hash_generator.rb
+++ lib/epay_tools/hash_generator.rb
@@ -15,19 +15,6 @@ class EpayTools::HashGenerator < ServicePattern::Service
 private
 
   def calculate_hash
-    @_calculate_hash ||= Digest::MD5.hexdigest("#{params_values.join}#{epay_md5_key}")
-  end
-
-  def params_values
-    unless @_params_values
-      @_params_values = {}
-      KEYS_ORDER.each do |key|
-        @_params_values[key] = params.delete(key) if params.key?(key)
-      end
-
-      @_params_values.merge!(params)
-    end
-
-    @_params_values.values
+    @_calculate_hash ||= Digest::MD5.hexdigest("#{params.values.join}#{epay_md5_key}")
   end
 end
